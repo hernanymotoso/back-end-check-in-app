e7df75b506f63d031f9a64d27fe287d028b71d95
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-07 20:27:17 -0300

    feat: Should be able to create a gym

---
+++ src/use-cases/create-gym/index.ts
@@ -0,0 +1,35 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+
+type CreateGymUseCaseRequest = {
+  title: string
+  description: string | null
+  phone: string | null
+  latitude: number
+  longitude: number
+}
+
+type CreateGymUseCaseResponse = {
+  gym: Gym
+}
+
+export class CreateGymUseCase {
+  constructor (private readonly gymsRepository: GymsRepository) {}
+
+  async execure ({ title, description, phone, latitude, longitude }: CreateGymUseCaseRequest): Promise<CreateGymUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude
+    })
+
+    if (!gym) throw new ResourceNotFoundError()
+
+    return {
+      gym
+    }
+  }
+}
