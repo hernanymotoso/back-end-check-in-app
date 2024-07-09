358e0d01b21c11a326d909f7a60db6dd4963aa3a
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-08 21:04:38 -0300

    feat: Get User Metrics Use Case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -23,6 +23,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.checkIns.filter(checkIn => checkIn.user_id === userId).slice((page - 1) * 20, page * 20)
   }
 
+  async countByUserId(userId: string) {
+    return this.checkIns.filter(checkIn => checkIn.user_id === userId).length
+  }
+
   async create (data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
---
+++ src/use-cases/get-user-metrics/index.ts
@@ -0,0 +1,21 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+type GetUserMetricsUseCaseRequest = {
+  userId: string
+}
+
+type GetUserMetricsUseCaseResponse = {
+  checkInsCount: number
+}
+
+export class GetUserMetricsUseCase {
+  constructor(private readonly checkInsRepository: CheckInsRepository) {}
+
+  async execute({ userId }: GetUserMetricsUseCaseRequest): Promise<GetUserMetricsUseCaseResponse> {
+    const checkInsCount = await this.checkInsRepository.countByUserId(userId)
+
+    return {
+      checkInsCount
+    }
+  }
+}
