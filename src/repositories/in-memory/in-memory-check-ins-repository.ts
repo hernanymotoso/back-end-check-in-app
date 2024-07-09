0ae2b2518f6121c181767ee6bbb7891f140a1810
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-08 21:57:00 -0300

    feat: Should be able to validate the check-in and Should not be able to validate an inexistent check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -27,6 +27,24 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.checkIns.filter(checkIn => checkIn.user_id === userId).length
   }
 
+  async findById(id: string) {
+    const checkIn = this.checkIns.find(checkIn => checkIn.id === id)
+
+    if (!checkIn) return null as any
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.checkIns.findIndex(currentCheckIn => currentCheckIn.id === checkIn.id)
+
+    if (checkInIndex >= 0) {
+      this.checkIns[checkInIndex] = checkIn
+    }
+
+    return checkIn
+  }
+
   async create (data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
