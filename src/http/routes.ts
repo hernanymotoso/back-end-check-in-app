f4b3174f7f59ae04d44bd17d2695240b8292b75c
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-09 21:30:14 -0300

    feat: create jwt middlerawe and set on me route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { type FastifyInstance } from 'fastify'
 import { register } from '@/http/controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes (app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.post('/me', { onRequest: [verifyJWT] }, profile)
 }
