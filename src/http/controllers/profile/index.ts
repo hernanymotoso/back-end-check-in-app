a5738ca8f3a7cb6b569cbc5e77d42df873e8659b
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-09 21:29:54 -0300

    feat:  implement profile controller

---
+++ src/http/controllers/profile/index.ts
@@ -0,0 +1,15 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/get-user-profile/factory'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { undefined } from 'zod'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
+}
