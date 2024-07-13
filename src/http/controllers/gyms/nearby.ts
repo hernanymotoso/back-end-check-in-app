586afa8568d1b324c143acadbc9dd3feb4efd671
Hernany Motoso <hernanymotoso@gmail.com>
2024-07-13 15:40:07 -0300

    feat: create gyms controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,23 @@
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/fetch-nearby-gyms/factory'
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function nearby (request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    })
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+  const { gyms } = await fetchNearbyGymsUseCase.execute({ userLatitude: latitude, userLongitude: longitude })
+
+  return reply.status(200).send({
+    gyms
+  })
+}
